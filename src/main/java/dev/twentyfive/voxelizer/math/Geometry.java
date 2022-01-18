07156ed51dc6aa2f4e47866ab1855fc24bc5967b
TwentyFive <TwentyFiveSoftware@gmail.com>
2022-01-18 17:43:39 +0100

    3d triangle rasterization

--- src/main/java/dev/twentyfive/voxelizer/math/Geometry.java
+++ src/main/java/dev/twentyfive/voxelizer/math/Geometry.java
@@ -8,7 +8,19 @@ import java.util.ArrayList;
 
 public class Geometry {
 
-    static boolean edgeFunction(Vector3 a, Vector3 b, Vector3 point) {
+    private static Vector3 calculateNormalizedPlaneNormal(Vector3 a, Vector3 b, Vector3 c) {
+        return Vector3.crossProduct(Vector3.sub(b, a), Vector3.sub(c, a)).normalize();
+    }
+
+    private static double calculateSignedDistancePlanePoint(Vector3 planeNormal, Vector3 planePoint, Vector3 point) {
+        return Vector3.dotProduct(planeNormal, (Vector3.sub(point, planePoint)));
+    }
+
+    private static Vector3 projectPointOnPlane(Vector3 planeNormal, Vector3 planePoint, Vector3 point) {
+        return Vector3.add(point, Vector3.multiply(planeNormal, -calculateSignedDistancePlanePoint(planeNormal, planePoint, point)));
+    }
+
+    private static boolean edgeFunction(Vector3 a, Vector3 b, Vector3 point) {
         return (point.x - a.x) * (b.y - a.y) - (point.y - a.y) * (b.x - a.x) >= 0;
     }
 
@@ -22,16 +34,30 @@ public class Geometry {
 
             int minX = (int) Math.min(a.x, Math.min(b.x, c.x));
             int minY = (int) Math.min(a.y, Math.min(b.y, c.y));
+            int minZ = (int) Math.min(a.z, Math.min(b.z, c.z));
+
             int maxX = (int) Math.max(a.x, Math.max(b.x, c.x));
             int maxY = (int) Math.max(a.y, Math.max(b.y, c.y));
+            int maxZ = (int) Math.max(a.z, Math.max(b.z, c.z));
+
+            Vector3 planeNormal = calculateNormalizedPlaneNormal(a, b, c);
 
             for (int x = minX; x <= maxX; x++) {
                 for (int y = minY; y <= maxY; y++) {
-                    Vector3 point = new Vector3(x, y, 0);
-                    boolean inside = edgeFunction(a, b, point) && edgeFunction(b, c, point) && edgeFunction(c, a, point);
+                    for (int z = minZ; z <= maxZ; z++) {
+                        Vector3 point = new Vector3(x, y, z);
 
-                    if (inside)
-                        voxels.add(new Voxel(new Vector3Int(x, 0, y), Material.RED_CONCRETE));
+                        double distance = Math.abs(calculateSignedDistancePlanePoint(planeNormal, a, point));
+                        if (distance > 0.5)
+                            continue;
+
+                        Vector3 projectedPoint = projectPointOnPlane(planeNormal, a, point);
+                        boolean insideTriangle = edgeFunction(a, b, projectedPoint) && edgeFunction(b, c, projectedPoint) && edgeFunction(c, a, projectedPoint);
+                        if (!insideTriangle)
+                            continue;
+
+                        voxels.add(new Voxel(new Vector3Int(x, y, z), Material.RED_CONCRETE));
+                    }
                 }
             }
         }
--- src/main/java/dev/twentyfive/voxelizer/math/Vector3.java
+++ src/main/java/dev/twentyfive/voxelizer/math/Vector3.java
@@ -9,6 +9,38 @@ public class Vector3 {
         this.z = z;
     }
 
+    public static Vector3 crossProduct(Vector3 a, Vector3 b) {
+        return new Vector3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+    }
+
+    public static double dotProduct(Vector3 a, Vector3 b) {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+
+    public double magnitude() {
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+    }
+
+    public Vector3 normalize() {
+        double magnitude = this.magnitude();
+        this.x /= magnitude;
+        this.y /= magnitude;
+        this.z /= magnitude;
+        return this;
+    }
+
+    public static Vector3 add(Vector3 a, Vector3 b) {
+        return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+
+    public static Vector3 sub(Vector3 a, Vector3 b) {
+        return new Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+
+    public static Vector3 multiply(Vector3 v, double scalar) {
+        return new Vector3(v.x * scalar, v.y * scalar, v.z * scalar);
+    }
+
     @Override
     public String toString() {
         return "(" + x + ", " + y + ", " + z + ")";
